637a570402d7413362c4b8195ab46e886ed85409
Fabian Linzberger <e@lefant.net>
2016-04-16 08:22:05 +0200

    more "configurable" test/client.rb

--- test/client.rb
+++ test/client.rb
@@ -1,10 +1,15 @@
 require 'json'
 require 'net/http'
 
+# host = 'localhost'
+# port = 8080
+host = 'whole-history-rating.herokuapp.com'
+port = 80
+
 file = File.read('test/games.json')
 games = JSON.parse(file)
 
 req = Net::HTTP::Post.new('/games', 'Content-Type' => 'application/json')
 req.body = games.to_json
-response = Net::HTTP.new('localhost', '8080').start { |http| http.request(req) }
+response = Net::HTTP.new(host, port).start { |http| http.request(req) }
 puts "Response #{response.code} #{response.message}: #{response.body}"
